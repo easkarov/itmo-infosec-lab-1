1027db76dde495c2696066a52e354df49780eace
e.askarov <askarov.emii@yandex.ru>
2025-10-05 23:42:47 +0300

    initial

--- src/main/java/se/ifmo/ru/lab1/controller/AuthController.java
+++ src/main/java/se/ifmo/ru/lab1/controller/AuthController.java
@@ -1,5 +1,7 @@
 package se.ifmo.ru.lab1.controller;
 
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 import se.ifmo.ru.lab1.dto.JwtResponse;
 import se.ifmo.ru.lab1.dto.LoginRequest;
 import se.ifmo.ru.lab1.dto.MessageResponse;
@@ -29,13 +31,17 @@ public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
     private final JwtUtils jwtUtils;
-    
+
+    private static final PolicyFactory POLICY = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        log.info("Login attempt for user: {}", loginRequest.getUsername());
+        String login = POLICY.sanitize(loginRequest.getUsername());
+
+        log.info("Login attempt for user: {}", login);
 
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+                new UsernamePasswordAuthenticationToken(login, loginRequest.getPassword()));
         
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateJwtToken(authentication);
@@ -47,22 +53,22 @@ public class AuthController {
     
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest signUpRequest) {
-        log.info("Registration attempt for user: {}", signUpRequest.getUsername());
-        
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+        String login = POLICY.sanitize(signUpRequest.getUsername());
+        log.info("Registration attempt for user: {}", login);
+        if (userRepository.existsByUsername(login)) {
             return ResponseEntity.badRequest()
                     .body(new MessageResponse("Error: Username is already taken!"));
         }
         
         User user = User.builder()
-                .username(signUpRequest.getUsername())
+                .username(login)
                 .password(encoder.encode(signUpRequest.getPassword()))
                 .role(User.Role.USER)
                 .enabled(true)
                 .build();
         
         userRepository.save(user);
-        log.info("User registered successfully: {}", user.getUsername());
+        log.info("User registered successfully: {}", login);
         
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
